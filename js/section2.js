fe5ba1fe2f69bf89a76c401b0ecd8de9df975db6
lmonteserin <lmonteserin@gmail.com>
2015-09-16 15:42:22 +0200

    FASDF

--- js/section2.js
+++ js/section2.js
@@ -149,7 +149,7 @@ Section2.prototype.clickBtn = function(ev)
 	alert("tt" +Number(this.totalCesta));*/
 		
 	var pago = paypalApp.pay(Number(this.totalCesta));			
-	alert("Longitud depago"+ pago.length);
+	alert("Longitud depago"+ pago);
 	
 	var urlService = 'http://1-dot-webgcommerceue.appspot.com/altaPedido';   
 	var ref = this;
